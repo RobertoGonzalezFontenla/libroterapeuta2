82315c1e3899dfc3c8a88071f97e81fab6179a54
Roberto González Lenovo <autonomo20@gmail.com>
2026-05-22 21:35:09 +0200

    fix: output server para cloudflare

--- src/lib/contentParser.ts
+++ src/lib/contentParser.ts
@@ -1,30 +1,21 @@
 ---
-import {
-  getCollection,
-  type CollectionEntry,
-  type CollectionKey,
-} from "astro:content";
+// src/lib/contentParser.ts
+import { getCollection, type CollectionKey, type CollectionEntry } from "astro:content";
 
-type PageData = {
-  title: string;
-  meta_title?: string;
-  description?: string;
-  image?: string;
+// Define una interfaz mínima para lo que esperas en 'data'
+interface PageData {
   draft?: boolean;
-};
+}
 
 export const getSinglePage = async <C extends CollectionKey>(
   collectionName: C
 ): Promise<CollectionEntry<C>[]> => {
   const allPages = await getCollection(collectionName);
-
   const removeIndex = allPages.filter((data) => data.id.match(/^(?!-)/));
 
-  const removeDrafts = removeIndex.filter((data) => {
+  return removeIndex.filter((data) => {
     const pageData = data.data as PageData;
     return pageData.draft !== true;
   });
-
-  return removeDrafts;
 };
 ---
